590dc7dc269f57486254a997c3c55f37ea6e68bc
oksana062018 <natina.o@mail.ru>
2024-08-29 07:26:30 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,18 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+len_list = len(my_list)
+x = 0
+while x != len_list:
+    if my_list[x]>0:
+        print(my_list[x])
+    elif my_list[x]<0:
+        break
+    x += 1
+
+
+
+
+
+
+
+
+
